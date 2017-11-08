8daec15f21b972492dc451bcb46eef1d43be70c3
Archangel37 <djarchangel3737@gmail.com>
2017-11-08 12:48:55 +0300

    solving Romanian converter

---
+++ Codewars_RomanNumeralsDecoder/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Codewars_RomanNumeralsDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.button_Decode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(52, 42);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(385, 20);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Location = new System.Drawing.Point(52, 98);
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.Size = new System.Drawing.Size(385, 20);
+            this.textBox_output.TabIndex = 0;
+            // 
+            // button_Decode
+            // 
+            this.button_Decode.Location = new System.Drawing.Point(479, 70);
+            this.button_Decode.Name = "button_Decode";
+            this.button_Decode.Size = new System.Drawing.Size(75, 23);
+            this.button_Decode.TabIndex = 1;
+            this.button_Decode.Text = "Decode";
+            this.button_Decode.UseVisualStyleBackColor = true;
+            this.button_Decode.Click += new System.EventHandler(this.button_Decode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 525);
+            this.Controls.Add(this.button_Decode);
+            this.Controls.Add(this.textBox_output);
+            this.Controls.Add(this.textBox_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_output;
+        private System.Windows.Forms.Button button_Decode;
+    }
+}
+
---
+++ Codewars_RomanNumeralsDecoder/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+//Description:
+
+//Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.You don't need to validate the form of the Roman numeral.
+
+//Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s.So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+//Example:
+
+//RomanDecode.Solution("XXI") -- should return 21
+//Courtesy of rosettacode.org
+
+//best solution:
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//public class RomanDecode
+//{
+//    private static Dictionary<char, int> _map3 = new Dictionary<char, int>
+//    {
+//        {'I',1},
+//        {'V',5},
+//        {'X',10},
+//        {'L',50},
+//        {'C',100},
+//        {'D',500},
+//        {'M',1000},
+//    };
+
+//    public static int Solution(string roman)
+//    {
+//        return roman.Reverse().Aggregate(0, (current, ch) => current + (_map3[ch] * 3 < current ? -_map3[ch] : _map3[ch]));
+//    }
+//}
+
+
+
+
+namespace Codewars_RomanNumeralsDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string RomanDecode(string input)
+        {
+            input = input.Trim();
+            List<char> romanian = new List<char>() {'I','V', 'X', 'L', 'C', 'D', 'M'};
+            List<int> numbers = new List<int>() { 1, 5, 10, 50, 100, 500, 1000 };
+            List<int> resultNums = new List<int>();
+            
+            foreach (char t in input)
+            {
+                if(romanian.Contains(t))
+                    resultNums.Add(numbers[romanian.IndexOf(t)]);
+            }
+
+            for (int k = resultNums.Count-1; k > 0; k--)
+            {
+                if (Math.Abs(resultNums[k]) > Math.Abs(resultNums[k - 1]))
+                    resultNums[k - 1] *= -1;
+            }
+            int Summ = 0;
+            foreach (var element in resultNums)
+            {
+                Summ += element;
+            }
+
+            return Summ.ToString();
+        }
+
+
+        private void button_Decode_Click(object sender, EventArgs e)
+        {
+            textBox_output.Text = RomanDecode(textBox_input.Text);
+        }
+    }
+}
